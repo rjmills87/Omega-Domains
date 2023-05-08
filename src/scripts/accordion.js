878553bd105a4612ec42a1d3843f46a0d172d70a
Rhys Miller <rjmills87@aol.com>
2023-05-08 20:45:34 +0100

    Javascript and style changes to accordion.

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,14 +1,13 @@
-const acc = document.querySelectorAll(".accordion");
-let i;
+const acc = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     const panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
