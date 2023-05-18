117f8250fe04e0b950fabff0e151131dbb2a99b6
Rhys Miller <rjmills87@aol.com>
2023-05-18 12:19:31 +0100

    Removal of active class to non active nav links

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -8,12 +8,18 @@ const emailAndMarketingMegaMenu = document.querySelector(".mega-menu-3");
 
 domainNamesBtn.addEventListener("click", function () {
   domainNamesMegaMenu.classList.toggle("active");
+  websitesAndHostingMegaMenu.classList.remove("active");
+  emailAndMarketingMegaMenu.classList.remove("active");
 });
 
 websitesAndHostingBtn.addEventListener("click", function () {
   websitesAndHostingMegaMenu.classList.toggle("active");
+  domainNamesMegaMenu.classlist.remove("active");
+  emailAndMarketingMegaMenu.classList.remove("active");
 });
 
 emailAndMarketingBtn.addEventListener("click", function () {
   emailAndMarketingMegaMenu.classList.toggle("active");
+  domainNamesMegaMenu.classList.remove("active");
+  websitesAndHostingMegaMenu.classList.remove("active");
 });
