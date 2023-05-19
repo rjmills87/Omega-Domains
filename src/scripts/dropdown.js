74647a81d8a4584f79280169118dfbae30fd9571
Rhys Miller <rjmills87@aol.com>
2023-05-19 20:16:15 +0100

    Changes to Mega Menu functionality.

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -2,23 +2,23 @@ const domainNamesBtn = document.querySelector(".header-link-1");
 const websitesAndHostingBtn = document.querySelector(".header-link-2");
 const emailAndMarketingBtn = document.querySelector(".header-link-3");
 
-const domainNamesMegaMenu = document.querySelector(".mega-menu-1");
-const websitesAndHostingMegaMenu = document.querySelector(".mega-menu-2");
-const emailAndMarketingMegaMenu = document.querySelector(".mega-menu-3");
+const domainNamesMegaMenu = document.getElementById("mega-menu-1");
+const websitesAndHostingMegaMenu = document.getElementById("mega-menu-2");
+const emailAndMarketingMegaMenu = document.getElementById("mega-menu-3");
 
-domainNamesBtn.addEventListener("click", function () {
+domainNamesBtn.addEventListener("click", () => {
   domainNamesMegaMenu.classList.toggle("active");
   websitesAndHostingMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
 });
 
-websitesAndHostingBtn.addEventListener("click", function () {
+websitesAndHostingBtn.addEventListener("click", () => {
   websitesAndHostingMegaMenu.classList.toggle("active");
-  domainNamesMegaMenu.classlist.remove("active");
+  domainNamesMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
 });
 
-emailAndMarketingBtn.addEventListener("click", function () {
+emailAndMarketingBtn.addEventListener("click", () => {
   emailAndMarketingMegaMenu.classList.toggle("active");
   domainNamesMegaMenu.classList.remove("active");
   websitesAndHostingMegaMenu.classList.remove("active");
