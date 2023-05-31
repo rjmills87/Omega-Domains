ec8a9cf0bf1663c88c4b7abd0399d3f6802a7269
Rhys Miller <rjmills87@aol.com>
2023-05-31 16:27:29 +0100

    Finished desktop styles to contact mega menu.

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,31 +1,44 @@
 const domainNamesBtn = document.querySelector(".header-link-1");
 const websitesAndHostingBtn = document.querySelector(".header-link-2");
 const emailAndMarketingBtn = document.querySelector(".header-link-3");
+const contactBtn = document.querySelector(".contact-link");
 
 const domainNamesMegaMenu = document.getElementById("mega-menu-1");
 const websitesAndHostingMegaMenu = document.getElementById("mega-menu-2");
 const emailAndMarketingMegaMenu = document.getElementById("mega-menu-3");
+const contactMegaMenu = document.getElementById("contact-menu");
 
 domainNamesBtn.addEventListener("click", () => {
   domainNamesMegaMenu.classList.toggle("active");
   websitesAndHostingMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
+  contactMegaMenu.classList.remove("active");
 });
 
 websitesAndHostingBtn.addEventListener("click", () => {
   websitesAndHostingMegaMenu.classList.toggle("active");
   domainNamesMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
+  contactMegaMenu.classList.remove("active");
 });
 
 emailAndMarketingBtn.addEventListener("click", () => {
   emailAndMarketingMegaMenu.classList.toggle("active");
   domainNamesMegaMenu.classList.remove("active");
   websitesAndHostingMegaMenu.classList.remove("active");
+  contactMegaMenu.classList.remove("active");
+});
+
+contactBtn.addEventListener("click", () => {
+  contactMegaMenu.classList.toggle("active");
+  domainNamesMegaMenu.classList.remove("active");
+  websitesAndHostingMegaMenu.classList.remove("active");
+  emailAndMarketingMegaMenu.classList.remove("active");
 });
 
 window.addEventListener("scroll", () => {
   domainNamesMegaMenu.classList.remove("active");
   websitesAndHostingMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
+  contactMegaMenu.classList.remove("active");
 });
