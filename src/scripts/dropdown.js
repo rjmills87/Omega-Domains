3dec8bb2b1fe310b2a5f5151bd3c5172dd1c3946
Rhys Miller <rjmills87@aol.com>
2023-05-31 19:23:20 +0100

    Finished Sign In dropdown menu.

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -2,17 +2,20 @@ const domainNamesBtn = document.querySelector(".header-link-1");
 const websitesAndHostingBtn = document.querySelector(".header-link-2");
 const emailAndMarketingBtn = document.querySelector(".header-link-3");
 const contactBtn = document.querySelector(".contact-link");
+const signInBtn = document.querySelector(".sign-in-link");
 
 const domainNamesMegaMenu = document.getElementById("mega-menu-1");
 const websitesAndHostingMegaMenu = document.getElementById("mega-menu-2");
 const emailAndMarketingMegaMenu = document.getElementById("mega-menu-3");
 const contactMegaMenu = document.getElementById("contact-menu");
+const signInMenu = document.getElementById("sign-in-menu");
 
 domainNamesBtn.addEventListener("click", () => {
   domainNamesMegaMenu.classList.toggle("active");
   websitesAndHostingMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
   contactMegaMenu.classList.remove("active");
+  signInMenu.classList.remove("active");
 });
 
 websitesAndHostingBtn.addEventListener("click", () => {
@@ -20,6 +23,7 @@ websitesAndHostingBtn.addEventListener("click", () => {
   domainNamesMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
   contactMegaMenu.classList.remove("active");
+  signInMenu.classList.remove("active");
 });
 
 emailAndMarketingBtn.addEventListener("click", () => {
@@ -27,6 +31,7 @@ emailAndMarketingBtn.addEventListener("click", () => {
   domainNamesMegaMenu.classList.remove("active");
   websitesAndHostingMegaMenu.classList.remove("active");
   contactMegaMenu.classList.remove("active");
+  signInMenu.classList.remove("active");
 });
 
 contactBtn.addEventListener("click", () => {
@@ -34,6 +39,15 @@ contactBtn.addEventListener("click", () => {
   domainNamesMegaMenu.classList.remove("active");
   websitesAndHostingMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
+  signInMenu.classList.remove("active");
+});
+
+signInBtn.addEventListener("click", () => {
+  signInMenu.classList.toggle("active");
+  domainNamesMegaMenu.classList.remove("active");
+  websitesAndHostingMegaMenu.classList.remove("active");
+  emailAndMarketingMegaMenu.classList.remove("active");
+  contactMegaMenu.classList.remove("active");
 });
 
 window.addEventListener("scroll", () => {
@@ -41,4 +55,5 @@ window.addEventListener("scroll", () => {
   websitesAndHostingMegaMenu.classList.remove("active");
   emailAndMarketingMegaMenu.classList.remove("active");
   contactMegaMenu.classList.remove("active");
+  signInMenu.classList.remove("active");
 });
