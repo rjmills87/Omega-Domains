94b1d4e2c199d8e407ca96096fbaf88184ae6e12
Rhys Miller <rjmills87@aol.com>
2023-07-03 10:14:42 +0100

    Mobile menu implemented.

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,5 +1,23 @@
-const hamburgerIcon = document.getElementById("hamburger");
+const mobileMenu = document.querySelector(".menu-container");
+const menuToggle = document.getElementById("hamburger");
+const body = document.querySelector("body");
 
-hamburgerIcon.addEventListener("click", function () {
-  hamburgerIcon.classList.toggle("open");
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+  menuToggle.classList.toggle("open");
+
+  if (
+    mobileMenu.classList.contains("active") &&
+    menuToggle.classList.contains("open")
+  ) {
+    body.style.overflow = "hidden";
+    body.addEventListener("touchmove", preventScroll);
+  } else {
+    body.style.overflow = "auto";
+    body.removeEventListener("touchmove", preventScroll);
+  }
 });
+
+function preventScroll(event) {
+  event.preventDefault();
+}
